d746a50fb8819d7eebf81ba7df37c3af9be3a71a
Andrea Carrara <andreacarrar@icloud.com>
2024-03-20 10:54:24 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module it.polimi.is24am05.is24am05 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens it.polimi.is24am05.is24am05 to javafx.fxml;
+    exports it.polimi.is24am05.is24am05;
+}
