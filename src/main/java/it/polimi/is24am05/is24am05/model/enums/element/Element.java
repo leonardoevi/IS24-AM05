369376a478e2ab747705431cc2360d5e0c67c737
Leonardo Evi <leonardo.evi@gmail.com>
2024-03-28 11:00:06 +0100

    Remove Placeholder files

--- src/main/java/it/polimi/is24am05/is24am05/model/enums/element/Element.java
+++
@@ -1,6 +0,0 @@
-package it.polimi.is24am05.is24am05.model.enums.element;
-
-/**
- * Implemented by Resource and Element to enable polymorphism.
- */
-public interface Element {}
--- src/main/java/it/polimi/is24am05/is24am05/model/playArea/PlayArea.java
+++
@@ -1,3 +0,0 @@
-package it.polimi.is24am05.is24am05.model.playArea;
-
-public class PlayArea {}
