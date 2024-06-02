48c3d39ecf6b710abffa9ace573aeb24fa23a526
Andrea Carrara <andreacarrar@icloud.com>
2024-06-02 18:32:11 +0200

    Print objectives in TUI [1]

--- src/main/java/it/polimi/is24am05/model/objective/Objective.java
+++ src/main/java/it/polimi/is24am05/model/objective/Objective.java
@@ -3,6 +3,7 @@ package it.polimi.is24am05.model.objective;
 import it.polimi.is24am05.model.playArea.PlayArea;
 
 import static it.polimi.is24am05.model.Player.HandDisplayer.matrixToString;
+import static it.polimi.is24am05.model.playArea.SideDisplayer.*;
 
 public enum Objective {
     /**
@@ -64,26 +65,98 @@ public enum Objective {
     }
 
     public String[][] toMatrix(){
-        // TODO : add colors and display each objective
+        // TODO : add colors
         String[][] matrix = new String[5][3];
 
         matrix[0][0] = " "; matrix[0][1] = "  ⌟" + getPoints() + "⌞  "; matrix[0][2] = " ";
         matrix[1][0] = matrix[1][2] = matrix[3][0] = matrix[3][2] = "+";
         matrix[2][0] = matrix[2][2] = "|";
-        matrix[4][0] = matrix[4][2] = " ";
-        matrix[4][1] = " " + this.name() + " ";
+        matrix[4][0] = matrix[4][2] = " "; matrix[4][1] = " " + this.name() + " ";
 
+        String f = RED_BOLD + "F" + RESET;
+        String p = GREEN_BOLD + "P" + RESET;
+        String a = CYAN_BOLD + "A" + RESET;
+        String i = PURPLE_BOLD + "P" + RESET;
         switch (this){
             case O_087:
-                matrix[1][1] = " " + "-".repeat(5) + " ";
-                matrix[2][1] = " ".repeat(7);
-                matrix[3][1] = " " + "-".repeat(5) + " ";
-
+                matrix[1][1] = "----" + f + "--";
+                matrix[2][1] = "   " + f + "   ";
+                matrix[3][1] = "--" + f + "----";
                 break;
-            default:
-                matrix[1][1] = matrix[3][1] = "-".repeat(7);
-                matrix[2][1] = " ".repeat(3) + "?" + " ".repeat(3);
-
+            case O_088:
+                matrix[1][1] = "--" + p + "----";
+                matrix[2][1] = "   " + p + "   ";
+                matrix[3][1] = "----" + p + "--";
+                break;
+            case O_089:
+                matrix[1][1] = "----" + a + "--";
+                matrix[2][1] = "   " + a + "   ";
+                matrix[3][1] = "--" + a + "----";
+                break;
+            case O_090:
+                matrix[1][1] = "--" + i + "----";
+                matrix[2][1] = "   " + i + "   ";
+                matrix[3][1] = "----" + i + "--";
+                break;
+            case O_091:
+                matrix[1][1] = "---" + f + "---";
+                matrix[2][1] = "   " + f + "   ";
+                matrix[3][1] = "----" + p + "--";
+                break;
+            case O_092:
+                matrix[1][1] = "---" + p + "---";
+                matrix[2][1] = "   " + p + "   ";
+                matrix[3][1] = "--" + i + "----";
+                break;
+            case O_093:
+                matrix[1][1] = "----" + f + "--";
+                matrix[2][1] = "   " + a + "   ";
+                matrix[3][1] = "---" + a + "---";
+                break;
+            case O_094:
+                matrix[1][1] = "--" + a + "----";
+                matrix[2][1] = "   " + i + "   ";
+                matrix[3][1] = "---" + i + "---";
+                break;
+            case O_095:
+                matrix[1][1] = "-------";
+                matrix[2][1] = " F F F ";
+                matrix[3][1] = "-------";
+                break;
+            case O_096:
+                matrix[1][1] = "-------";
+                matrix[2][1] = " P P P ";
+                matrix[3][1] = "-------";
+                break;
+            case O_097:
+                matrix[1][1] = "-------";
+                matrix[2][1] = " A A A ";
+                matrix[3][1] = "-------";
+                break;
+            case O_098:
+                matrix[1][1] = "-------";
+                matrix[2][1] = " I I I ";
+                matrix[3][1] = "-------";
+                break;
+            case O_099:
+                matrix[1][1] = "-------";
+                matrix[2][1] = " Q I M ";
+                matrix[3][1] = "-------";
+                break;
+            case O_100:
+                matrix[1][1] = "-------";
+                matrix[2][1] = "  M M  ";
+                matrix[3][1] = "-------";
+                break;
+            case O_101:
+                matrix[1][1] = "-------";
+                matrix[2][1] = "  I I  ";
+                matrix[3][1] = "-------";
+                break;
+            case O_102:
+                matrix[1][1] = "-------";
+                matrix[2][1] = "  Q Q  ";
+                matrix[3][1] = "-------";
                 break;
         }
 
