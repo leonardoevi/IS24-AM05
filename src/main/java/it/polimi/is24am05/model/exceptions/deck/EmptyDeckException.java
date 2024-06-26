bfc449d25d3df1f5f04d840bddd3eabca84f4720
Leonardo <leonardo.evi@gmail.com>
2024-06-26 14:26:16 +0200

    Add empty deck log message

--- src/main/java/it/polimi/is24am05/model/exceptions/deck/EmptyDeckException.java
+++ src/main/java/it/polimi/is24am05/model/exceptions/deck/EmptyDeckException.java
@@ -4,4 +4,8 @@ package it.polimi.is24am05.model.exceptions.deck;
  * Exception thrown when trying to draw from a deck when there are no more covered cards, regardless of the visible cards.
  */
 public class EmptyDeckException extends Exception{
+    @Override
+    public String getMessage() {
+        return "Deck is empty";
+    }
 }
